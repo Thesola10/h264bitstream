d0ad3003cf4feffda1922859bb2f56815f1dd017
TheSola10 <me@thesola.io>
2023-08-23 22:45:28 +0200

    Added Nix stuff

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs."nixpkgs".url = github:nixos/nixpkgs;
+
+  outputs = { self, nixpkgs, ... }:
+  { packages.x86_64-linux.default =
+      (import nixpkgs { system = "x86_64-linux"; }).stdenv.mkDerivation {
+        name = "h264bitstream";
+        src = ./.;
+        buildPhase = "make -f Makefile.unix";
+        installPhase = "mkdir -p $out/lib && install -Dm755 libh264bitstream.a $out/lib";
+      };
+  };
+}
