8a3176569e489ccff86601d3686abbc299aec47d
TheSola10 <me@thesola.io>
2023-08-25 18:04:06 +0200

    Added ARM option

--- flake.nix
+++ flake.nix
@@ -9,5 +9,12 @@
         buildPhase = "make -f Makefile.unix";
         installPhase = "mkdir -p $out/lib && install -Dm755 libh264bitstream.a $out/lib";
       };
+    packages.aarch64-linux.default =
+      (import nixpkgs { system = "aarch64-linux"; }).stdenv.mkDerivation {
+        name = "h264bitstream";
+        src = ./.;
+        buildPhase = "make -f Makefile.unix";
+        installPhase = "mkdir -p $out/lib && install -Dm755 libh264bitstream.a $out/lib";
+      };
   };
 }
